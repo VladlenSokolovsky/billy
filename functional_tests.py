8c27957a87711289c3a20650e5025d421059b0e7
VladlenSokolovsky <vladlensokolovskii@gmail.com>
2022-04-28 10:31:41 +0300

    Before big refactor

--- functional_tests.py
+++ functional_tests.py
@@ -28,26 +28,34 @@ class NewVisitorsTest(unittest.TestCase):
 
         # It is immediately inviting her to enter the item of list
         inputbox = self.browser.find_element_by_id('id_new_item')
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
+
+        # self.assertEqual(
+        #     inputbox.get_attribute('placeholder'),
+        #     'Enter a to-do item'
+        # )
 
         # She enters in the text field "Buy peacock feathers" (her hobby is
         # fly knitting)
-        inputbox.send_keys('Buy peacock feathers')
+        # inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys('Use peacock feathers to make a fly')
 
         # When she is pressing enter, the page is updating, and now the page
         # contains "1: Buy peacock feathers" like item of the list
         inputbox.send_keys(Keys.ENTER)
         time.sleep(3)
 
-        table = self.browser.find_element_by_id('id_new_item')
-        rows = self.browser.find_elements_by_id('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item not appear in table"
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
         )
+        # self.assertTrue(
+        #     any(row.text == '1: Buy peacock feathers' for row in rows),
+        #     f"New to-do item not appear in table. It was:\
+        # n{table.text}"
+        # )
 
         # The text field still inviting her to add one more item
         # She enters "To do the peacock feather fly"
