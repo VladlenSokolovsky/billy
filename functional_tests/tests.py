b4c52ce2f8838093854b8013b945a8ea130ba385
VladlenSokolovsky <vladlensokolovskii@gmail.com>
2022-05-03 08:57:53 +0300

    tests for different users and url

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -31,7 +31,7 @@ class NewVisitorTest(LiveServerTestCase):
                     raise e
                 time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user_self(self):
         # Edith heard about new app for list
         # urgent tasks. She is deciding to rate her homepage
         self.browser.get(self.live_server_url)
@@ -63,8 +63,8 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # The page is updating again, and now displays the both of the elements of her list
-        self.wait_for_row_in_list_table('1: Buy peacock feathers')
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
         # Edith is interesting, if the site will remember her list?
         # The site generated the unique URL
         # About this displaying the text with explanations
@@ -72,4 +72,45 @@ class NewVisitorTest(LiveServerTestCase):
         # She is visiting the URL - her list is still there.
 
         # Satisfied, she goes back to sleep
-        self.fail('Finish the test')
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Edith starts a new list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
+
+        # She notes that her list have the unique URL
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
+
+        # Now new visitor come to the site.
+
+        # # We use a new session of browser to provide that any information
+        # # from Edith not will pass from cookies etc.
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits the homepage. There are not any attribute of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('Use peacock feathers to make a fly', page_text)
+
+        # Francis starts a new list with a new item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy some milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy some milk')
+
+        # Francis get the unique URL:
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # There are not any attributes of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy some milk', page_text)
+
+        # Satisfied they go back to sleep
