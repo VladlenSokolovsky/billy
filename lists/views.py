a264a618df5bbaf586bcb8321212f631d2ec08a4
VladlenSokolovsky <vladlensokolovskii@gmail.com>
2022-05-03 14:37:55 +0300

    Made the model of list, added URL for creating a new list by post

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 
 def home_page(request):
@@ -7,8 +7,8 @@ def home_page(request):
 
 
 def new_list(request):
-
-    Item.objects.create(text=request.POST['item_text'])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect('/lists/the-only-lists-of-its-kind-in-the-world/')
 
 
