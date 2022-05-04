bd5ee3f7a9ebf4afe8a6af6cce9cc740877aa0d6
VladlenSokolovsky <vladlensokolovskii@gmail.com>
2022-05-04 16:43:37 +0300

    Used Bootstrap to upgrade the layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,5 +1,5 @@
 from selenium.common.exceptions import WebDriverException
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -8,7 +8,7 @@ import time
 MAX_WAIT = 10
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     """Test of the new visitor"""
 
     def setUp(self):
