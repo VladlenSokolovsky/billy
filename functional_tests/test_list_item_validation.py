d7b4e989fdbd642e9ee77924b76f8806db46c00c
VladlenSokolovsky <vladlensokolovskii@gmail.com>
2022-05-10 14:10:53 +0300

    Moved unit-tests to dir tests

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,8 +1,28 @@
 from .base import FunctionalTest
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 
 class ItemValidationTest(FunctionalTest):
 
     def test_cannot_add_empty_list_item(self):
-        self.fail('Write something!')
+
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element(By.CSS_SELECTOR, '.has-error').text,
+            "You can't have an empty list item"
+        ))
+        self.browser.find_element(By.ID, 'id_new_item').send_keys('Buy milk')
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element(By.CSS_SELECTOR, '.has-error').text,
+            "You can't have an empty list item"
+        ))
+
+        self.browser.find_element(By.ID, 'id_new_item').send_keys('Green tea')
+        self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        self.wait_for_row_in_list_table('2: Green tee')
